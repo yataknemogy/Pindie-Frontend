8cd95eceb2e92553c6d5ebbec3348c35706a7ceb
yataknemogy <efim@yataknemogy.com>
2024-05-23 15:20:21 +0400

    Add files via upload

--- my-app/app/components/AuthForm/AuthForm.jsx
+++ my-app/app/components/AuthForm/AuthForm.jsx
@@ -1,71 +1,74 @@
-'use client';
-import { authorize, isResponseOk} from '@/app/api/api-utils';
-import Styles from './AuthForm.module.css';
-import { useState, useEffect } from 'react';
-import { endpoints } from '@/app/api/config';
-import { useContext } from 'react';
-import { AuthContext } from '@/app/context/app-context';
-import { useStore } from '@/app/store/app-store';
+"use client";
+import Styles from "./AuthForm.module.css";
+import { useState, useEffect } from "react";
+import { endpoints } from "@/app/api/config";
+import { authorize } from "@/app/api/api-utils";
+import { isResponseOk } from "@/app/api/api-utils";
+import { useStore } from "@/app/store/app-store";
 
 export const AuthForm = (props) => {
-  const [authData, setAuthData] = useState({ identifier: "", password: "" })
-  const [message, setMessage] = useState({ status: null, text: null});
-  
-  const authContext=useStore();
-
- const handleInput = (e)=>{
-  setAuthData({ ...authData, [e.target.name]: e.target.value });
- };
- const handleSubmit = async (e)=>{
+  const authContext = useStore();
+  const [authData, setAuthData] = useState({ identifier: "", password: "" });
+  const [message, setMessage] = useState({ status: null, text: null });
+  const handleInput = (e) => {
+    setAuthData({ ...authData, [e.target.name]: e.target.value });
+  };
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    const userDataAuth = await authorize(endpoints.auth, authData);
-    if(isResponseOk(userDataAuth)){
-      authContext.login(userDataAuth.user, userDataAuth.jwt);
+    const userData = await authorize(endpoints.auth, authData);
+    if (isResponseOk(userData)) {
+      authContext.login(userData.user, userData.jwt);
       setMessage({ status: "success", text: "Вы авторизовались!" });
+    } else {
+      setMessage({ status: "error", text: "Неверные почта или пароль" });
     }
-    else{
-      setMessage({ status: "error", text: "Неверный логин или пароль" });
+  };
+  useEffect(() => {
+    let timer;
+    if (authContext.user) {
+      timer = setTimeout(() => {
+        setMessage({ status: null, text: null });
+        props.close();
+      }, 1000);
     }
- }
-    useEffect(() =>{
-      let timer;
-      if(authContext.user){
-       timer = setTimeout(() =>{
-          props.closePopup();
-        },1000)
-      }
-      return () => clearTimeout(timer)
-    },[authContext.user])
-
-    
+    return () => clearTimeout(timer);
+  }, [authContext.user]);
   return (
-    <form  onSubmit={handleSubmit} className={Styles['form']}>
-      <h2 className={Styles['form__title']}>Авторизация</h2>
-      <div className={Styles['form__fields']}>
-        <label className={Styles['form__field']}>
-          <span className={Styles['form__field-title']}>Email</span>
-          <input onInput={handleInput} 
-          className={Styles['form__field-input']} 
-          name="identifier"
-          type="email" 
-          placeholder="hello@world.com"/>
+    <form onSubmit={handleSubmit} className={Styles["form"]}>
+      <h2 className={Styles["form__title"]}>Авторизация</h2>
+      <div className={Styles["form__fields"]}>
+        <label className={Styles["form__field"]}>
+          <span className={Styles["form__field-title"]}>Email</span>
+          <input
+            onInput={handleInput}
+            className={Styles["form__field-input"]}
+            name="identifier"
+            type="email"
+            placeholder="hello@world.com"
+          />
         </label>
-        <label className={Styles['form__field']}>
-          <span className={Styles['form__field-title']}>Пароль</span>
-          <input onInput={handleInput}
-           className={Styles['form__field-input']} 
-           name="password"
-           type="password"
-           placeholder='***********'/>
+        <label className={Styles["form__field"]}>
+          <span className={Styles["form__field-title"]}>Пароль</span>
+          <input
+            onInput={handleInput}
+            className={Styles["form__field-input"]}
+            type="password"
+            name="password"
+            placeholder="***********"
+          />
         </label>
       </div>
-      {message.status &&
-      <p className={Styles['form__message']}>{message.text}</p>
-      }
-      <div className={Styles['form__actions']}>
-        <button className={Styles['form__reset']} type="reset">Очистить</button>
-        <button className={Styles['form__submit']} type="submit">Войти</button>
+      {message.status && (
+        <p className={Styles["form__message"]}>{message.text}</p>
+      )}
+      <div className={Styles["form__actions"]}>
+        <button className={Styles["form__reset"]} type="reset">
+          Очистить
+        </button>
+        <button className={Styles["form__submit"]} type="submit">
+          Войти
+        </button>
       </div>
     </form>
-  ) 
+  );
 };
