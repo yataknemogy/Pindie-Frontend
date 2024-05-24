15d207e98ca91e6da19539a84ab89d5e5d265714
yataknemogy <efim@yataknemogy.com>
2024-05-24 11:19:24 +0400

    Add files via upload

--- my-app/app/api/config.js
+++ my-app/app/api/config.js
@@ -1,7 +1,7 @@
-export const BASE_URL = 'http://localhost:3001/api';
+export const BASE_URL = 'https://pindie.portal.nomoredomainswork.ru/';
 
 export const endpoints = {
     games: `${BASE_URL}/games`,
-    auth: `${BASE_URL}/auth/local`,
-    me: `${BASE_URL}/users/me`,
+    auth: `${BASE_URL}/auth/login`,
+    me: `${BASE_URL}/login/me`,
 }
